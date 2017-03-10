3dbb8652c3fb5a817f74a6514a64450e355a0767
browna38 <ajbrown000@gmail.com>
2017-03-10 11:41:12 -0600

    Add files via upload

---
+++ Autonomous_Robot.ino
@@ -0,0 +1,98 @@
+/*
+  Analog input, analog output, serial output
+
+ Reads an analog input pin, maps the result to a range from 0 to 255
+ and uses the result to set the pulsewidth modulation (PWM) of an output pin.
+ Also prints the results to the serial monitor.
+
+ The circuit:
+ * potentiometer connected to analog pin 0.
+   Center pin of the potentiometer goes to the analog pin.
+   side pins of the potentiometer go to +5V and ground
+ * LED connected from digital pin 9 to ground
+
+ created 29 Dec. 2008
+ modified 9 Apr 2012
+ by Tom Igoe
+
+ This example code is in the public domain.
+
+ */
+#include <Servo.h>
+// These constants won't change.  They're used to give names
+// to the pins used:
+const int analogInPin = A0;  // Analog input pin that the potentiometer is attached to
+const int analogOutPin = 9; // Analog output pin that the LED is attached to
+Servo servoLeft;          // Define left servo
+Servo servoRight;         // Define right servo
+
+int sensorValue = 0;        // value read from the pot
+int outputValue = 0;        // value output to the PWM (analog out)
+int outputValue2 = 0;
+void setup() {
+  // initialize serial communications at 9600 bps:
+  Serial.begin(9600);
+  servoLeft.attach(2);  // Set left servo to digital pin 10
+  servoRight.attach(3);  // Set right servo to digital pin 9
+}
+
+void loop() {
+  // read the analog in value:
+  sensorValue = analogRead(analogInPin);
+  // map it to the range of the analog out:
+  outputValue = map(sensorValue, 0, 25, 0, 1);
+
+
+  if (outputValue == 0){
+    stopRobot();
+    delay(500);
+    reverse();
+    delay(500);
+    turnRight();
+    delay(250);
+  } else {
+    forward();             // Example: move forward
+    delay(500);           // Wait 2000 milliseconds (2 seconds)
+  }
+  // change the analog out value:
+  analogWrite(analogOutPin, outputValue);
+
+  // print the results to the serial monitor:
+  Serial.print("sensor = ");
+  Serial.print(sensorValue);
+  Serial.print("\toutputValue2 = ");
+  Serial.print(outputValue2);
+  Serial.print("\t output = ");
+  Serial.println(outputValue);
+
+  // wait 2 milliseconds before the next loop
+  // for the analog-to-digital converter to settle
+  // after the last reading:
+  delay(2);
+
+  
+}
+
+void forward() {
+  servoLeft.write(0);
+  servoRight.write(180);
+}
+
+void reverse() {
+  servoLeft.write(180);
+  servoRight.write(0);
+}
+
+void turnRight() {
+  servoLeft.write(180);
+  servoRight.write(180);
+}
+void turnLeft() {
+  servoLeft.write(0);
+  servoRight.write(0);
+}
+
+void stopRobot() {
+  servoLeft.write(90);
+  servoRight.write(90);
+}
